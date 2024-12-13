b028fef6031ec9607c4df5d83dbb63371eab87b4
a-bermudez <abermudeztroncoso@gmail.com>
2024-12-13 13:42:18 +0100

    take assets from the template of gitHub and make the necessary components

--- inflow-and-outflow/src/app/app.module.ts
+++ inflow-and-outflow/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InflowComponent } from './components/inflow-outflow/inflow/inflow.component';
+import { OutflowComponent } from './components/inflow-outflow/outflow/outflow.component';
+import { InflowOutflowComponent } from './components/inflow-outflow/inflow-outflow.component';
+import { StatsComponent } from './components/inflow-outflow/stats/stats.component';
+import { DetailsComponent } from './components/inflow-outflow/details/details.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    InflowComponent,
+    OutflowComponent,
+    InflowOutflowComponent,
+    StatsComponent,
+    DetailsComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ inflow-and-outflow/src/app/components/inflow-outflow/inflow-outflow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inflow-outflow',
+  templateUrl: './inflow-outflow.component.html',
+  styleUrls: ['./inflow-outflow.component.scss']
+})
+export class InflowOutflowComponent {
+
+}
